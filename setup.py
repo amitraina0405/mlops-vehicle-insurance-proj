1516d72fd970b803a646a772f59af836570f72d1
Amit Raina <write2amitr@yahoo.com>
2025-08-12 14:38:29 +0530

    create folders and sub folders

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="amit raina",
+    author_email="amit.raina86@aol.com",
+    packages=find_packages()
+)
